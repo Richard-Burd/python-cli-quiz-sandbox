3faddc0850bcadef6ec52b7701cb0838793729eb
Richard-Burd <qalbalassadalasfour@gmail.com>
2019-09-03 16:45:12 -0700

    add inheritance to Student class

---
+++ Global.py
@@ -0,0 +1,5 @@
+class GlobalTracker:
+    
+    def global_tracking_device(self):
+    	print("The global tracker can be accessed by any student because of class inheritance")
+
--- Objects.py
+++ Objects.py
@@ -4,7 +4,13 @@ class Question:
 		self.prompt = prompt
 		self.answer = answer
 
-class Student: 
+# Python won't read the whole script before executing it
+# You need to import the <<GlobalTracker>> class even though it's in this very file
+# You could also define the <<GlobalTracker>> class before defining the <<Student>> class
+from Global import GlobalTracker
+
+# The <<Student>> class will inherit from the <<GlobalTracker>> class
+class Student(GlobalTracker): 
 
 	def __init__(self, name, major, gpa, is_on_probation):
 		self.name = name
@@ -12,9 +18,18 @@ class Student:
 		self.gpa = gpa
 		self.is_on_probation = is_on_probation
 
+
+
 class Book: 
 
 	def __init__(self, title, author, owner):
 		self.title = title
 		self.owner = owner
 		self.author = author
+
+# This is a repeat of what is in the Global.py file...sort of...but it is ignored by the program.
+class GlobalTracker:
+    
+    def global_tracking_device(self):
+    	print("This instance of GlobalTracker is completely ignored by the program")
+
